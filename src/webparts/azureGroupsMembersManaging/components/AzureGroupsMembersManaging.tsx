bb1dc29853fce92f890782ba498f529b086b0516
Oleksii Marchenko <omarchenko@it-dev.pl>
2024-04-24 20:10:01 +0200

    added 'removing user from group' functionality | BE

--- src/webparts/azureGroupsMembersManaging/components/AzureGroupsMembersManaging.tsx
+++ src/webparts/azureGroupsMembersManaging/components/AzureGroupsMembersManaging.tsx
@@ -1,30 +1,33 @@
-import * as React from 'react';
+import * as React from "react";
 // import styles from './AzureGroupsMembersManaging.module.scss';
 // import type { IAzureGroupsMembersManagingProps } from './IAzureGroupsMembersManagingProps';
 // import { escape } from '@microsoft/sp-lodash-subset';
 
-import GetMembers from './GetMembers/GetMembers';
+import GetMembers from "./GetMembers/GetMembers";
 
+import GetGroups from "./GetGroups/GetGroups";
+import AddMember from "./AddMember/AddMember";
+import RemoveMember from "./RemoveMember/RemoveMember";
 
-import GetGroups from './GetGroups/GetGroups';
-import AddMember from './AddMember/AddMember';
+function AzureGroupsMembersManaging(props: any) {
+  const { context } = props;
+  console.log(context.pageContext.user.displayName);
 
-
-function AzureGroupsMembersManaging(props:any){
-const {context} = props;
-console.log(context.pageContext.user.displayName);
-
-    return (
-     <section>
+  return (
+    <section>
+      Welcome, {context.pageContext.user.displayName}
+      <h2> Get groups</h2>
+      <GetGroups context={context} />
+      <h2> Get members</h2>
+      <GetMembers context={props.context} />
       
-     Welcome,  {context.pageContext.user.displayName}
+      <h2> Modify members</h2>
+      <AddMember context={context} />
 
-   <GetMembers context = {props.context}/>
-   <GetGroups context = {context}/>
-   <AddMember context = {context}/>
-     </section>
-      
-    );
+      <h2>Remove User</h2>
+      <RemoveMember context = {context} />
+    </section>
+  );
 }
 
 export default AzureGroupsMembersManaging;
---
+++ src/webparts/azureGroupsMembersManaging/services/removeMember.ts
@@ -0,0 +1,18 @@
+import { MSGraphClient } from "@microsoft/sp-http";
+
+
+export const removeMemberService = (context: any, groupId:string, userId:string) => {
+   
+
+    return context.msGraphClientFactory
+        .getClient()
+        .then((client: MSGraphClient) => {
+            return client
+            .api(`/groups/${groupId}/members/${userId}/$ref`)
+                .version('v1.0')
+                .delete();
+        })
+        .then((response: any) => {
+            return response.value;
+        });
+};
