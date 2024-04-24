235db987acf8e665230ffc474dc5562fc6d2b16f
Oleksii Marchenko <omarchenko@it-dev.pl>
2024-04-24 14:20:03 +0200

    Initial commit

---
+++ src/webparts/azureGroupsMembersManaging/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IAzureGroupsMembersManagingWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'AzureGroupsMembersManagingWebPartStrings' {
+  const strings: IAzureGroupsMembersManagingWebPartStrings;
+  export = strings;
+}
