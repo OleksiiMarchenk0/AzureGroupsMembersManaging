5a49a57a346b1461e83aaf340813ef61ed3f08e4
Oleksii Marchenko <omarchenko@it-dev.pl>
2024-04-25 18:10:42 +0200

    poc get filtered groups - step 2

--- src/webparts/azureGroupsMembersManaging/components/GetGroups/GetGroups.tsx
+++ src/webparts/azureGroupsMembersManaging/components/GetGroups/GetGroups.tsx
@@ -10,8 +10,6 @@ import { WebPartContext } from "@microsoft/sp-webpart-base";
 // import { GetOwnedGroups } from "../../helper/GetOwnedGroups,";
 // import { WebPartContext } from "@microsoft/sp-webpart-base";
 
-
-
 export default function GetGroups(props: any) {
   const [groups, setGroups] = React.useState<any[]>([]);
   const [filteredGroups, setFilteredGroups] = React.useState<any[]>([]);
@@ -20,7 +18,14 @@ export default function GetGroups(props: any) {
   const templateRoleId = "62e90394-69f5-4237-9190-012177145e10";
   const [isAdmin, setIsAdmin] = React.useState<boolean | null>(null);
   const { context } = props;
-//const geID = '5c1aabf4-973a-4b60-bce1-6bf364ed5437';
+
+  console.log(filteredGroups);
+  
+  //const geID = '5c1aabf4-973a-4b60-bce1-6bf364ed5437';
+
+
+
+
   // set isAdmin
   useEffect(() => {
     async function checkAdminStatus() {
@@ -39,23 +44,7 @@ export default function GetGroups(props: any) {
     checkAdminStatus();
   }, [context, templateRoleId]);
 
-  //Getting filteredGroups
- useEffect(()=>{
-  const fetchFilteredGroups = async (context:WebPartContext, groups:any) => {
-    try {
-      const filteredGroupsData = await GetOwnedGroups(context,groups);
-      console.log(filteredGroupsData);
-      setFilteredGroups(filteredGroups)
-      
-    } catch (error) {
-      console.error("Error fetching groups:", error);
-    } finally {
-     // setLoading(false);
-    }
-  };
 
-  fetchFilteredGroups(context,groups);
- },[isAdmin])
 
   //Getting all groups
   useEffect(() => {
@@ -75,35 +64,74 @@ export default function GetGroups(props: any) {
     };
 
     fetchAllGroups();
-  }, [isAdmin]);
+  }, []);
 
 
+  //Getting filteredGroups
+  useEffect(() => {
+      const fetchFilteredGroups = async (
+	 context: WebPartContext
+	) => {
+    try {
+      const { owners, groupsData } = await GetOwnedGroups(context);
+      console.log(owners);
+      console.log(groupsData);
+      if(groupsData){
+        setFilteredGroups(groupsData)
+      }
+   
+      // Use the owners and groupsData as needed
+    } catch (error) {
+      console.error("Error occurred while fetching owned groups:", error);
+    } finally {
+        setLoading(false);
+      }
+    };
 
+    fetchFilteredGroups(context);
+  }, []);
   return (
     <div>
       {loading ? (
         <div>Loading groups...</div>
       ) : (
         <div>
-          <h2> Get groups</h2>
-          {filteredGroups.map((group: IGroupProps) => (
-            <div>
-              <Button
-                onClick={() => {
-                  props.getChosenGroupId(group.id);
-                }}
-              >
-                {group.displayName}
-              </Button>
-            </div>
-          ))}
-
           {isAdmin === null ? (
             <p>Loading...</p>
           ) : isAdmin ? (
-            <p>The user is an admin.</p>
+            <>
+              {" "}
+              <p>The user is an admin.</p>
+              <h2> Get global admin groups</h2>
+              {groups.map((group: IGroupProps) => (
+                <div>
+                  <Button
+                    onClick={() => {
+                      props.getChosenGroupId(group.id);
+                    }}
+                  >
+                    {group.displayName}
+                  </Button>
+                </div>
+              ))}
+            </>
           ) : (
-            <p>The user is not an admin.</p>
+            <>
+              <p>The user is not an admin.</p>
+              <h2> Get country admin groups</h2>
+              {filteredGroups.map((filteredGroup: IGroupProps) => (
+                <div>
+                  <Button
+                    onClick={() => {
+                      props.getChosenGroupId(filteredGroup.id);
+                    }}
+                  >
+                    {filteredGroup.displayName}
+                  </Button>
+                </div>
+              ))}
+            </>
+          
           )}
         </div>
       )}
