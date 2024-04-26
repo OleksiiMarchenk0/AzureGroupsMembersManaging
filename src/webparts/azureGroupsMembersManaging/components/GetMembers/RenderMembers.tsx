eea0e4bce84ca3e9c64af4d56659b9cd528f3506
Oleksii Marchenko <omarchenko@it-dev.pl>
2024-04-26 22:21:40 +0200

    added filter for members

--- src/webparts/azureGroupsMembersManaging/components/GetMembers/RenderMembers.tsx
+++ src/webparts/azureGroupsMembersManaging/components/GetMembers/RenderMembers.tsx
@@ -1,33 +1,51 @@
 import * as React from "react";
-import { Text } from "@fluentui/react";
-import { IListMembersProps } from './IListMembersProps';
-import { Persona, DefaultButton } from '@fluentui/react';
-import { mergeStyles } from '@fluentui/react/lib/Styling';
-import styles from './RenderMembers.module.scss'; // Import the SCSS file
+import { Text, TextField } from "@fluentui/react";
+import { IListMembersProps } from "./IListMembersProps";
+import { Persona, DefaultButton } from "@fluentui/react";
+import { mergeStyles } from "@fluentui/react/lib/Styling";
+import styles from "./RenderMembers.module.scss"; // Import the SCSS file
 
 export default function RenderMembers(props: IListMembersProps) {
   const { members, removeUser, isGroupChosen } = props;
+  const [searchText, setSearchText] = React.useState<string>("");
 
+
+  // Filter members based on search text
+  const filteredMembers = searchText
+    ? members.filter(member => member.displayName.toLowerCase().indexOf(searchText.toLowerCase()) !== -1)
+    : members;
+
+    
   return (
     <>
       {isGroupChosen && (
         <>
           <h2>Members of the group</h2>
           {members && members.length > 0 ? (
-            members.map((member: any) => (
-              <div key={member.id} className={mergeStyles(styles.memberContainer)}>
-                <Persona
-                  imageUrl={member.imageUrl} // Provide the URL of the user's image
-                  text={member.displayName} // Display name of the user
-                  secondaryText={member.jobTitle} // Job title or secondary information
-                />
-                <DefaultButton
-                  onClick={() => removeUser(member.id)} // Function to remove the user
-                  iconProps={{ iconName: 'Cancel' }} // Use the Cancel icon
-                  className={styles.cancelButton} // Apply the styles from SCSS
-                />
-              </div>
-            ))
+            <>
+              <TextField
+                label="Search members"
+                value={searchText}
+                onChange={(event, newValue) => setSearchText(newValue || "")}
+              />
+              {filteredMembers.map((member: any) => (
+                <div
+                  key={member.id}
+                  className={mergeStyles(styles.memberContainer)}
+                >
+                  <Persona
+                    imageUrl={member.imageUrl} 
+                    text={member.displayName}
+                    secondaryText={member.jobTitle} 
+                  />
+                  <DefaultButton
+                    onClick={() => removeUser(member.id)} 
+                    iconProps={{ iconName: "Cancel" }} 
+                    className={styles.cancelButton} 
+                  />
+                </div>
+              ))}
+            </>
           ) : (
             <Text>No members found</Text>
           )}
