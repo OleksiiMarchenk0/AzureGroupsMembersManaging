9ca4c4477f1b444a7cb77a915673cc23fb053aca
Oleksii Marchenko <omarchenko@it-dev.pl>
2024-04-26 20:42:45 +0200

    poc styles for showing groups in ui

--- src/webparts/azureGroupsMembersManaging/components/GetGroups/IListGroupsProps.ts
+++ src/webparts/azureGroupsMembersManaging/components/GetGroups/IListGroupsProps.ts
@@ -1,4 +1,12 @@
-interface IListGroupsProps{
-    groups:[];
-    getChosenGroupId: CallableFunction;
-}
+// IListGroupsProps interface
+export interface IListGroupsProps {
+    groups: IGroup[];
+    getChosenGroupId: (id: string) => void;
+  }
+  
+  // IGroup interface
+  export interface IGroup {
+    id: string;
+    displayName: string;
+    // Add more properties as needed
+  }
--- src/webparts/azureGroupsMembersManaging/components/GetGroups/RenderGroups.tsx
+++ src/webparts/azureGroupsMembersManaging/components/GetGroups/RenderGroups.tsx
@@ -1,23 +1,39 @@
-import * as React from 'react'
-import { IGroupProps } from './IGroupProps';
-import { Button } from "@fluentui/react-components";
+import * as React from 'react';
+//import { DetailsList, DetailsListLayoutMode, SelectionMode } from '@fluentui/react';
+//import { mergeStyles } from '@fluentui/react/lib/Styling';
+//import { List } from '@fluentui/react/lib/List';
+import { IGroup, IListGroupsProps } from './IListGroupsProps';
+import styles from './RenderGroups.module.scss';
+import { Stack, Text } from '@fluentui/react';
 
-export default function RenderGroups(props:IListGroupsProps) {
-  const {groups, getChosenGroupId} = props;
-  return (
-    <div>
-   <h2> Avaliable groups</h2>
-    {groups.map((group: IGroupProps) => (
-      <div>
-        <Button
-          onClick={() => {
-            getChosenGroupId(group.id);
-          }}
-        >
-          {group.displayName}
-        </Button>
+
+export default function RenderGroups(props: IListGroupsProps) {
+
+  const [selectedGroupId, setSelectedGroupId] = React.useState<string | null>(null);
+  const { groups, getChosenGroupId } = props;
+  const handleGroupClick = (groupId: string) => {
+    getChosenGroupId(groupId); // Notify parent component of the selected group ID
+    setSelectedGroupId(groupId); // Update the selected group ID in state
+    console.log('Selected Group ID:', groupId); // Log the selected group ID to the console
+  };
+  const renderGroupItem = (group: IGroup, index: number) => {
+    const isSelected = selectedGroupId === group.id;
+    return (
+      <div
+        key={group.id}
+        className={`${styles.listItem} ${isSelected ? styles.selected : ''}`}
+        onClick={() => handleGroupClick(group.id)}
+      >
+        <Text as="div" className={styles.groupTitle}>{group.displayName}</Text>
       </div>
-    ))}
+    );
+  };
+  return (
+    <div className={styles.container}>
+      <h2>Available groups</h2>
+      <Stack horizontal wrap tokens={{ childrenGap: 10 }}>
+        {groups.map(renderGroupItem)}
+      </Stack>
     </div>
-  )
+  );
 }
