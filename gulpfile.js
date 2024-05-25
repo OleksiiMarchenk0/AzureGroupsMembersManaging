6ee6c20b3beb3e4b450e501e1ee356791a9d9727
Oleksii Marchenko <omarchenko@it-dev.pl>
2024-05-25 15:35:50 +0200

    ci/cd

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 'use strict';
 
 const build = require('@microsoft/sp-build-web');
+const deploy = require('@pnp/spfx-node');
+const path = require('path');
+const packageSolution = require('@microsoft/sp-build-web/lib/tasks/package-solution/package-solution');
+const build = require('@microsoft/sp-build-web');
 
 build.addSuppression(`Warning - [sass] The local CSS class 'ms-Grid' is not camelCase and will not be type-safe.`);
 
@@ -14,3 +18,19 @@ build.rig.getTasks = function () {
 };
 
 build.initialize(require('gulp'));
+
+
+exports.deploy = series(build.tasks.packageSolution.packageSolutionTask, () => {
+  const deployOptions = {
+    siteUrl: process.env.TENANT_URL,
+    creds: {
+      clientId: process.env.CLIENT_ID,
+      certificatePath: 'cert.pfx',  // This should match the file name used in the workflow
+      certificatePassword: process.env.CERT_PASSWORD,
+      tenantId: process.env.TENANT_ID
+    }
+  };
+
+  return src('sharepoint/solution/*.sppkg')
+    .pipe(deploySPFx(deployOptions));
+});
